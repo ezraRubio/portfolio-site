09272e1ea2fe9935e89340609feef11ddb3ad816
ezraRubio <ezra@ezrarubio.com>
2023-03-05 16:32:44 +0200

    tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 import React from "react";
-import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="">
-      <Head>
-        <title>Ezra Rubio : Developer</title>
-        <meta name="description" content="Ezra's portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="">
-        <h1 className="">Welcome to My Portfolio</h1>
-        <p>...which is still in progress, check back again soon.</p>
-      </main>
-    </div>
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>
+      <p>...which is still in progress, check back again soon.</p>
+    </main>
   );
 }
