72d15927c7fa6cd6da9e5608fe9ebdc5c91ee8a0
ezraRubio <ezra@ezrarubio.com>
2023-03-05 15:35:26 +0200

    migrated to app dir

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Ezra Rubio : Developer</title>
+        <meta name="description" content="Ezra's portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </>
+    )
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
+import React from "react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="">
       <Head>
         <title>Ezra Rubio : Developer</title>
         <meta name="description" content="Ezra's portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to My Portfolio</h1>
+      <main className="">
+        <h1 className="">Welcome to My Portfolio</h1>
         <p>...which is still in progress, check back again soon.</p>
       </main>
     </div>
