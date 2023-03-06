381f2c5209497f3980d173a330f9db8ebabc6adc
Ezra <81970147+ezraRubio@users.noreply.github.com>
2023-03-06 17:49:32 +0200

    title
    * navigation
    
    * styled navigation
    
    * added title

--- src/components/NavBar/Navigator.tsx
+++ src/components/NavBar/Navigator.tsx
@@ -1,14 +1,20 @@
+import { title } from "@/models/constants";
 import { routes } from "@/models/enums";
 import LinkedRoute from "./LinkedRoute";
 
 function Navigator() {
-  const navStyle = "flex flex-row items-center justify-end mt-1 space-x-6";
+  const navStyle = "flex flex-row items-center justify-end mt-1 space-x-6 mr-2";
+  const titleStyle = "-mb-8 text-xl text-bold mt-1 ml-2" 
+  
   return (
-    <ul className={navStyle}>
-      {(Object.values(routes) as string[])?.map((route) => (
-        <LinkedRoute route={route} key={route} />
-      ))}
-    </ul>
+    <>
+      <h1 className={titleStyle}>{ title }</h1>
+      <ul className={navStyle}>
+        {(Object.values(routes) as string[])?.map((route) => (
+          <LinkedRoute route={route} key={route} />
+        ))}
+      </ul>
+    </>
   );
 }
 
