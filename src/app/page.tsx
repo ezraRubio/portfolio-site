ea42ec1f5f12e095645ebc3aadcb413fb8630b7f
ezra <ezra@ezrarubio.com>
2023-03-06 13:48:25 +0200

    upgrades done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>
+      <h1 className="text-6xl font-bold p-5">Welcome to My Portfolio</h1>
       <p>...which is still in progress, check back again soon.</p>
     </main>
   );
