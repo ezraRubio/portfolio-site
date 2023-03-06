d5f111889d3bca915bd6083eb405b59099a3b7c8
Ezra <81970147+ezraRubio@users.noreply.github.com>
2023-03-06 13:49:12 +0200

    Feature/upgrade dependencies (#1)
    * migrated to app dir
    
    * tailwind css
    
    * node 19
    
    * downgrade node to fix build issue
    
    * add engine specification
    
    * making tailwind to work
    
    * upgrades done

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold p-5">Welcome to My Portfolio</h1>
+      <p>...which is still in progress, check back again soon.</p>
+    </main>
+  );
+}
