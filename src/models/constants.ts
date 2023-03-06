e956caef2fd0a7bf2cf2fc07a4c4e9d128804767
ezra <ezra@ezrarubio.com>
2023-03-06 17:47:21 +0200

    added title

---
+++ src/models/constants.ts
@@ -0,0 +1,1 @@
+export const title = "Ezra Rubio : Developer";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //       sans: [
-  //         "-apple-system",
-  //         "BlinkMacSystemFont",
-  //         "Segoe UI",
-  //         "Roboto",
-  //         "Oxygen",
-  //         "Ubuntu",
-  //         "Cantarell",
-  //         "Fira Sans",
-  //         "Droid Sans",
-  //         "Helvetica Neue",
-  //         "sans-serif",
-  //       ],
-  //     },
-  //     fontWeight: {
-  //       regular: 400,
-  //       bold: 700,
-  //     },
-  //     fontStyle: {
-  //       normal: "normal",
-  //       italic: "italic",
-  //       bold: "bold",
-  //     },
-  //   },
-  // },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Fira Sans",
+          "Droid Sans",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
+      },
+      fontWeight: {
+        regular: 400,
+        bold: 700,
+      },
+      fontStyle: {
+        normal: "normal",
+        italic: "italic",
+        bold: "bold",
+      },
+    },
+  },
   plugins: [],
 };
