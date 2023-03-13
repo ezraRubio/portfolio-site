c2414bc9d745827e68488e5c021f14c9edae769e
ezra <ezra@ezrarubio.com>
2023-03-13 10:00:08 +0200

    coming soon fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,12 @@
 function Contact() {
-  return <h2 className="m-10">comming soon</h2>;
+  return (
+    <>
+      <h2 className="mt-28">coming soon</h2>
+      <h3> Ezra Rubio </h3>
+      <h3> ezra@ezrarubio.com</h3>
+      <h3> 0585504711 </h3>
+    </>
+  );
 }
 
 export default Contact;
