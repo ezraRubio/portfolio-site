b7f0dfff88eb83763afab7ad7afdf9fdfc865ebf
ezraRubio <ezra@ezrarubio.com>
2023-03-18 23:14:48 +0200

    added external links

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,26 @@
 function Contact() {
   return (
-    <>
+    <div className="ml-8">
       <h2 className="mt-28">coming soon</h2>
       <h3> Ezra Rubio </h3>
       <h3> ezra@ezrarubio.com</h3>
       <h3> 0585504711 </h3>
-    </>
+      <div className="flex flex-row space-x-4">
+        <a
+          href="https://www.linkedin.com/in/ezrarubio/"
+          className="bg-blue-500 hover:bg-blue-700 text-white text-center font-semibold py-2 px-4 rounded w-1/6"
+        >
+          Linked In
+        </a>
+        <a
+          href="https://github.com/ezraRubio"
+          className="bg-blue-500 hover:bg-blue-700 text-white text-center font-semibold py-2 px-4 rounded w-1/6"
+        >
+          {" "}
+          GitHub
+        </a>
+      </div>
+    </div>
   );
 }
 
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,25 @@
 function Projects() {
   return (
-    <>
+    <div className="ml-8">
       <h2 className="mt-28">coming soon</h2>
       <h3> Ezra Rubio </h3>
       <h3> ezra@ezrarubio.com</h3>
       <h3> 0585504711 </h3>
-    </>
+      <div className="flex flex-col space-y-6 mt-6">
+        <h1 className="text-bold text-black text-3xl">Pet Agency Project: </h1>
+        <a
+          href="https://pet-agency.vercel.app/"
+          target="_blank"
+          className="bg-blue-500 hover:bg-blue-700 text-white text-center font-semibold py-2 px-4 rounded w-1/6"
+        >
+          Click Here
+        </a>
+        <p>
+          * currently undergoing extensive refactoring, but search functionality
+          is available *
+        </p>
+      </div>
+    </div>
   );
 }
 
