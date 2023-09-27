3736058d09235cda9c7d642f810c1af2b6c1dfbf
Ezra <ezra@ezrarubio.com>
2023-09-27 22:34:41 +0300

    added project image component

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,7 +1,10 @@
+import { StaticImageData } from "next/image";
+
 export type Project = {
     title: string;
     repo: string;
     description: string;
+    images?: StaticImageData[];
     downloadOrOpen?: string;
     youtubeId?: string;
     isHosted?: boolean;
