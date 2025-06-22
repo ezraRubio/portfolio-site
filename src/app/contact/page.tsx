14b755dac181a8685598b5a24467b5486b27fe3d
Ezra <81970147+ezraRubio@users.noreply.github.com>
2025-06-22 09:51:09 +0300

    temporary: remove contact details

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,12 +6,6 @@ import { ButtonOptions } from "@/models/enums";
 function Contact() {
   return (
     <div className="ml-10 mt-20">
-      <h2 className="font-semibold"> Ezra Rubio </h2>
-      <div className="m-6">
-        For inquiries:
-        <p className="font-medium"> ezra@ezrarubio.com</p>
-        <p className="font-medium"> +972585504711 </p>
-      </div>
       <div className="flex flex-row space-x-4">
         <Button link={LINKEDIN} title={ButtonOptions.LINKEDIN} />
         <Button link={GITHUB} title={ButtonOptions.GITHUB} />
