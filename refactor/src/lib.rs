7cdf8202b2c593f1bc2009b37c8707eea5340893
ezraRubio <ezra@ezrarubio.com>
2026-05-19 22:29:43 +0300

    connected main

--- refactor/src/lib.rs
+++ refactor/src/lib.rs
@@ -5,6 +5,7 @@ mod app;
 use leptos::*;
 use leptos_router::*;
 use leptos_meta::*;
+use wasm_bindgen::prelude::wasm_bindgen;
 use components::nav_bar::navigator::Navigator;
 
 #[component]
@@ -12,14 +13,13 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <Stylesheet id="leptos" href="/pkg/portfolio.css"/>
         <Title text="Ezra Rubio : Developer" />
         <Meta name="description" content="Ezra's portfolio" />
         <Link rel="icon" href="/favicon.ico" />
-        <Navigator />
         <Router>
+            <Navigator />
             <Routes>
-                <Route path="" view=app::home::Home />
+                <Route path="/" view=app::home::Home />
                 <Route path="projects" view=app::projects::Projects />
                 <Route path="contact" view=app::contact::Contact />
             </Routes>
@@ -27,12 +27,20 @@ pub fn App() -> impl IntoView {
     }
 }
 
+#[wasm_bindgen(start)]
 pub fn main() {
-    console_log::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
+    console_error_panic_hook::set_once();
+    web_sys::console::log_1(&"=== main started ===".into());
 
+    console_log::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
+    web_sys::console::log_1(&"logging initialized".into());
+
+    web_sys::console::log_1(&"calling mount_to_body".into());
     mount_to_body(|| {
+        web_sys::console::log_1(&"inside mount_to_body closure".into());
         view! {
             <App />
         }
     });
+    web_sys::console::log_1(&"mount_to_body returned".into());
 }
